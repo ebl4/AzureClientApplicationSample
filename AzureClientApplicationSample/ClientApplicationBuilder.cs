cb8fad1b7bbc2d6c2854aeabb361b936b5c21afa
Edson <edbdelima@gmail.com>
2023-03-03 11:41:42 -0300

    Adicionar arquivos de projeto.

---
+++ AzureClientApplicationSample/ClientApplicationBuilder.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Identity.Client;
+
+namespace AzureClientApplicationSample
+{
+    public class ClientApplicationBuilder
+    {
+        private readonly string _clientSecret;
+        private readonly string _clientId;
+
+        public ClientApplicationBuilder(string clientSecret, string clientId)
+        {
+            _clientSecret = clientSecret;
+            _clientId = clientId;
+        }
+
+        public void InitializeConfidentialClientApplication()
+        {
+            string redirectUri = "https://myapp.azurewebsites.net";
+            var app = ConfidentialClientApplicationBuilder.Create(_clientId)
+                .WithClientSecret(_clientSecret)
+                .WithRedirectUri(redirectUri)
+                .Build();
+        }
+        
+        public void InitializePublicClientApplication()
+        {
+            var app = PublicClientApplicationBuilder.Create(_clientId).Build();
+        }
+    }
+}
